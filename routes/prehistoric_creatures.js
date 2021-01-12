eb735546779737d9e709541986f44f5d90c2bc3a
Gavin Callander <gavincallander@MacBook-Pro.local>
2021-01-12 14:59:52 -0500

    updatese

---
+++ routes/prehistoric_creatures.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+// Mounted at /prehistoric_creatures
+let historics = fs.readFileSync('./prehistoric_creatures.json');
+let histData = JSON.parse(historics);
+
+// Index - /prehistoric_creatures
+router.get('/', (req, res) => {
+    res.render('historics/index', { historics: histData })
+});
+// New - prehistoric_creatures/new
+router.get('/new', (req, res) => {
+    res.render('historics/new');
+});
+// Get 1 - /prehistoric_creatures/1
+router.get('/:id', (req, res) => {
+    let index = req.params.id;
+    res.render('historics/show', { historic: histData[index] });
+});
+// Post - prehistoric_creatures
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+// Requirements
+const express = require('express');
+const layouts = require('express-ejs-layouts');
+const path = require('path');
+
+//  App Setup
+const app = express();
+app.set('view engine', 'ejs');
+app.use(layouts);
+app.use(express.static(path.join(__dirname, '/static')));
+app.use(express.urlencoded({ extended: false })); //Body parsing middleware
+
+// Global routes
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+// Controllers/routes
+app.use('/dinos', require('./routes/dinos'));
+app.use('/prehistoric_creatures', require('./routes/prehistoric_creatures'));
+
+// Listen
+app.listen(8000, () => console.log('🧚‍♂️ Hey! Listen! 🧚‍♀️'));
